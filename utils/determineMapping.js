2df7fe828b14a7da703eb2d406e54d0de6f02ca1
Will Koste <willkoste@gmail.com>
2021-04-15 16:22:56 -0400

    Switch statement- make sure mapKey is String

--- utils/determineMapping.js
+++ utils/determineMapping.js
@@ -9,20 +9,23 @@ const determineMapping = async (aptTypeID) => {
 			apiKey2: mapping.endpoint2.apiKey
 		};
 
-		let mapKey = mapping._id;
+		let mapKey = String(mapping._id);
 
 		console.log({mapKey});
 
-		if (mapKey === '607883220268d225705304c9') {
-			return {
-				...info,
-				type2: mapping.appointmentType2
-			};
-		} else if (mapKey === '607883bad4fddd6d4074cf90') {
-			return {
-				...info,
-				type2: mapping.appointmentType2
-			};
+		switch (mapKey) {
+			case '607883220268d225705304c9':
+				return {
+					...info,
+					type2: mapping.appointment2
+				};
+			case '607883bad4fddd6d4074cf90':
+				return {
+					...info,
+					type2: mapping.appointment2
+				};
+			default:
+				return 'default';
 		}
 	} catch (err) {
 		console.error(err);
