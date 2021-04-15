7f267e7526246e8c5bc05b2bb05cc1ade4403bb4
Will Koste <willkoste@gmail.com>
2021-04-15 15:49:58 -0400

    Working on the mapping logic. Appt-Types to be more specific

--- routes/api/mapping.js
+++ routes/api/mapping.js
@@ -31,7 +31,7 @@ router.get('/', async (req, res) => {
 // @access  Admin
 router.get('/2', async (req, res) => {
 	try {
-		const mapping = await Mapping.findOne({appointmentType1: '21572963'}).populate({path: 'endpoint1 account1 endpoint2 account2'});
+		const mapping = await Mapping.findOne({appointmentType1: 21572963}).populate({path: 'endpoint1 account1 endpoint2 account2'});
 
 		if (!mapping) {
 			return res.status(404).json({success: false, mappings: `Mappings could not be found, please try again`});
--- utils/determineMapping.js
+++ utils/determineMapping.js
@@ -1,6 +1,6 @@
 const Mapping = require('../models/Mapping');
 
-const determineMapping = async (aptTypeID, calID) => {
+const determineMapping = async (aptTypeID) => {
 	try {
 		const mapping = await Mapping.findOne({appointmentType1: aptTypeID});
 
@@ -11,17 +11,16 @@ const determineMapping = async (aptTypeID, calID) => {
 
 		let mapKey = mapping._id;
 
-		switch (mapKey) {
-			case '607883220268d225705304c9':
-				return {
-					...info,
-					type2: mapping.appointmentType2
-				};
-			case '607883bad4fddd6d4074cf90':
-				return {
-					...info,
-					type2: mapping.appointmentType2
-				};
+		if (mapKey === '607883220268d225705304c9') {
+			return {
+				...info,
+				type2: mapping.appointmentType2
+			};
+		} else if (mapKey === '607883bad4fddd6d4074cf90') {
+			return {
+				...info,
+				type2: mapping.appointmentType2
+			};
 		}
 	} catch (err) {
 		console.error(err);
