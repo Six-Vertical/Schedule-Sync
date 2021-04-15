7d8d20046af59bc4d2c0a9a7de310aacae00d801
Will Koste <willkoste@gmail.com>
2021-04-15 10:22:29 -0400

    Adjusted the production static spec

--- server.js
+++ server.js
@@ -30,11 +30,10 @@ app.use('/api/v1/ma/mapping', require('./routes/api/mapping'));
 app.use('/api/v1/ma/calendars', require('./routes/api/calendars'));
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, './', 'client', 'src')));
-	// app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
+	app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
