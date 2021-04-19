a8bcff7375cce8d79d2fc5f1a05a3c8c9b1bb9b5
Will Koste <willkoste@gmail.com>
2021-04-19 13:35:43 -0400

    admin-true

--- routes/acuity/acuity.js
+++ routes/acuity/acuity.js
@@ -232,7 +232,7 @@ router.post(`/appointments/create`, async (req, res) => {
 
 				var options = {
 					headers: {'content-type': 'application/json'},
-					url: 'https://acuityscheduling.com/api/v1/appointments',
+					url: 'https://acuityscheduling.com/api/v1/appointments?admin=true',
 					auth: {
 						user: mappingKey.userId2,
 						password: mappingKey.apiKey2
@@ -303,7 +303,7 @@ router.post(`/appointments/create/d2`, async (req, res) => {
 
 				var options = {
 					headers: {'content-type': 'application/json'},
-					url: 'https://acuityscheduling.com/api/v1/appointments',
+					url: 'https://acuityscheduling.com/api/v1/appointments?admin=true',
 					auth: {
 						user: mappingKey.userId2,
 						password: mappingKey.apiKey2
