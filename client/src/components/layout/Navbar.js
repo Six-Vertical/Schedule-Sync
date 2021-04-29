7f9818a7268dc6410e0a414b108871a3b37abee1
Will Koste <willkoste@gmail.com>
2021-04-29 14:58:18 -0400

    Updated the navbars look/order

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -5,24 +5,24 @@ const Navbar = () => {
 	return (
 		<div className='bg-light border-right' id='sidebar-wrapper'>
 			<ul className='list-group list-group-flush'>
-				<li className='list-group-item'>
+				<li className='list-group-item text-center'>
 					<NavLink exact to='/' activeClassName='current' className='text-dark p-1'>
 						Accounts
 					</NavLink>
 				</li>
-				<li className='list-group-item'>
+				<li className='list-group-item text-center'>
 					<NavLink to='/endpoints' activeClassName='current' className='text-dark p-1'>
 						Endpoints
 					</NavLink>
 				</li>
-				<li className='list-group-item'>
-					<NavLink to='/mapping' activeClassName='current' className='text-dark p-1'>
-						Apt Types
-					</NavLink>
-				</li>
-				<li className='list-group-item'>
+				<li className='list-group-item text-center'>
 					<NavLink to='/calendars' activeClassName='current' className='text-dark p-1'>
 						Calendars
+					</NavLink>
+				</li>
+				<li className='list-group-item text-center'>
+					<NavLink to='/mapping' activeClassName='current' className='text-dark p-1'>
+						Appointment Types
 					</NavLink>
 				</li>
 			</ul>
